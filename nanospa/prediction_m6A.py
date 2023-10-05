94e62b87ca5f111cf231934b6fe181e235d991e7
sihaohuanguc <shuang1024@uchicago.edu>
2023-10-05 02:34:44 -0500

    update

--- nanospa/prediction_m6A.py
+++ nanospa/prediction_m6A.py
@@ -18,54 +18,59 @@ __status__ = "Development"
 def pred_m6A(in_name,out_name):
     working_path=os.getcwd()
     if not in_name:
-        in_folder=working_path+"/alignment"
-        in_file=in_folder+"/features_GGACU.csv"
+        in_folder=working_path+"/alignment/features"
         if not out_name:
             out_prediction_file=working_path+"/alignment/prediction_m6A.csv"
         else:
             out_prediction_file=out_name
     else:
-        if "/" in in_name:
-            in_folder="/".join(in_name.split("/")[:-1])
-        else:
-            in_folder=working_path
-        in_file=in_name
+        in_folder=in_name.rstrip("/")
         if not out_name:
             out_prediction_file=in_folder+"/prediction_m6A.csv"
         else:
             out_prediction_file=out_name
+    
+    motif_list=["GGACT","AGACT","GGACA","TGACT","GGACC","AGACA","AGACC","GGACG"]
 
-    df=pd.read_csv(in_file) # this table has header line
-    cols=["gene_ID","position","base_type","coverage"]
-    site_info=df[cols]
-    df.drop(["gene_ID","position","base_type","coverage","mis_1","mis_2","mis_3","mis_4","mis_5"],axis=1,inplace=True)  # remove all overall mismatch features
+    all_results=pd.DataFrame(columns=["gene_ID","position","base_type","coverage","modi"])
+    for motif in motif_list:
 
-    scaler=MinMaxScaler()
-    scaler.fit(df)
-    df_standarized=pd.DataFrame(scaler.transform(df),columns=df.columns)
+        in_file=in_folder+f"/features_{motif}.csv"
+        if os.path.exists(in_file): # some motifs may not exist
+            df=pd.read_csv(in_file)
+            cols=["gene_ID","position","base_type","coverage"]
+            site_info=df[cols]
+            df.drop(["gene_ID","position","base_type","coverage","mis_1","mis_2","mis_3","mis_4","mis_5"],axis=1,inplace=True)  # remove all overall mismatch features
 
-    model=tf.keras.models.Sequential([
-        tf.keras.layers.Dense(128,activation="relu",input_shape=(55,)),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(64,activation="relu"),
-        tf.keras.layers.Dropout(0.1),
-        tf.keras.layers.Dense(2,activation="softmax"),   # this is the output layer
-    ])
+            scaler=MinMaxScaler()
+            scaler.fit(df)
+            df_standarized=pd.DataFrame(scaler.transform(df),columns=df.columns)
 
-    base_path=os.path.abspath(__file__)
-    folder=os.path.dirname(base_path)
-    model_path=folder+"/data/model/m6A_model_1_0_GGACU/cp.ckpt"
-    if os.path.exists(model_path+".index"):
-        print("Load the model.")
-        model.load_weights(model_path).expect_partial()
-    else:
-        print("Model for m6A prediction not exist.")
-        exit(100)
+            model=tf.keras.models.Sequential([
+                tf.keras.layers.Dense(128,activation="relu",input_shape=(60,)),
+                tf.keras.layers.Dropout(0.2),
+                tf.keras.layers.Dense(64,activation="relu"),
+                tf.keras.layers.Dropout(0.1),
+                tf.keras.layers.Dense(2,activation="softmax"),   # this is the output layer
+            ])
 
-    prediction=model.predict(df_standarized)
-    pred_df=pd.DataFrame(prediction,columns=["unmodi","modi"])
-    site_info=site_info.reset_index(drop=True).join(pred_df["modi"]) # you need to reset the index of site_info df
-    site_info.to_csv(out_prediction_file,index=False,header=None)
+            base_path=os.path.abspath(__file__)
+            folder=os.path.dirname(base_path)
+            model_path=folder+f"/data/model/m6A_models/m6A_model_2_0_{motif}/cp.ckpt"
+            if os.path.exists(model_path+".index"):
+                print("Load the model. ", motif)
+                model.load_weights(model_path).expect_partial()
+                # model.load_weights(model_path)
+            else:
+                print("Model for m6A prediction not exist.")
+                exit(100)
+
+            prediction=model.predict(df_standarized)
+            pred_df=pd.DataFrame(prediction,columns=["unmodi","modi"])
+            site_info=site_info.reset_index(drop=True).join(pred_df["modi"]) # you need to reset the index of site_info df
+            all_results=pd.concat([all_results,site_info],axis=0)
+    
+    all_results.to_csv(out_prediction_file,index=False,header=None)
 
 
 
--- nanospa/preprocess_m6A.py
+++ nanospa/preprocess_m6A.py
@@ -19,65 +19,72 @@ def pre_m6A(in_name,out_name):
         in_folder=working_path+"/alignment"
         in_file=in_folder+"/features.csv"
         if not out_name:
-            out_file=working_path+"/alignment/features_GGACU.csv"
+            out_folder=working_path+"/alignment/features"
         else:
-            out_file=out_name
+            out_folder=out_name.rstrip("/")
     else:
         if "/" in in_name:
-            in_folder="/".join(in_name.split("/")[:-1])
+            in_folder="/".join(in_name.split("/")[:-1])  # to remove file name
         else:
             in_folder=working_path
         in_file=in_name
         if not out_name:
-            out_file=in_folder+"/features_GGACU.csv"
+            out_folder=in_folder+"/features"
         else:
-            out_file=out_name
-    temp_file=in_folder+"/features_GGACU_temp.csv" 
+            out_folder=out_name.rstrip("/")
+    temp_folder=in_folder+"/features_temp"
 
-    motif="GGACT" # use T instead of U !!!
-    modification_index=2 # this is the position-1 of the modification site A in the motif GGACT
+    if not os.path.exists(out_folder):
+        os.makedirs(out_folder)
+    if not os.path.exists(temp_folder):
+        os.makedirs(temp_folder)
 
-    queue_base=[]  # for bases
-    queue_position=[]  # for the positions
-    queue_info=[]  # for all the info
-    have_write_something=False
-    with open(temp_file,"w+") as out_f:
-        writer=csv.writer(out_f)
-        with open(in_file,"r") as inf:
-            reader=csv.reader(inf)
-            for line in reader:        
-                queue_base.append(line[2])
-                queue_position.append(int(line[1]))
-                queue_info.append(line)
-                if len(queue_base)==len(motif): # now we have enough bases
-                    if "".join(queue_base)==motif:  # it is a motif needed
-                        if [x-queue_position[0] for x in queue_position]==[i for i in range(len(motif))]: # the positions are neighbors, and the difference will be [0,1,2,3,4] for 5 mers
-                            # print(queue_info)
-                            out_line=queue_info[modification_index][:4] # the base info of the modification position
-                            for info in queue_info:
-                                out_line.extend(info[4:])
-                            writer.writerow(out_line)
-                            have_write_something=True
-                            
-                    queue_base.pop(0)  # pop out the first site
-                    queue_position.pop(0)
-                    queue_info.pop(0)
+    motif_list=["GGACT","AGACT","GGACA","TGACT","GGACC","AGACA","AGACC","GGACG"]
+    
+    for motif in motif_list:
+        temp_file=temp_folder+f"/features_temp_{motif}.csv"
+        out_file=out_folder+f"/features_{motif}.csv"
+        modification_index=2 # this is the position-1 of the modification site A in the motif GGACT
 
-    # there are too many columns, let's generate the name of the columns
-    cols=["gene_ID","position","base_type","coverage"]
-    base_col=["ins","ins_len","del","del_len","del_site","mis","mis_A","mis_C","mis_G","mis_T","base_qual_mean","base_qual_STD","base_qual_count_0"]
-    for i in range(1,6):
-        for item in base_col:
-            cols.append(f"{item}_{i}")
-    # print(cols)
-    if have_write_something:
-        df=pd.read_csv(temp_file,header=None)
-        df.columns=cols
+        queue_base=[]  # for bases
+        queue_position=[]  # for the positions
+        queue_info=[]  # for all the info
+        have_write_something=False
+        with open(temp_file,"w+") as out_f:
+            writer=csv.writer(out_f)
+            with open(in_file,"r") as inf:
+                reader=csv.reader(inf)
+                for line in reader:        
+                    queue_base.append(line[2])
+                    queue_position.append(int(line[1]))
+                    queue_info.append(line)
+                    if len(queue_base)==len(motif): # now we have enough bases
+                        if "".join(queue_base)==motif:  # it is a motif needed
+                            if [x-queue_position[0] for x in queue_position]==[i for i in range(len(motif))]: # the positions are neighbors, and the difference will be [0,1,2,3,4] for 5 mers
+                                # print(queue_info)
+                                out_line=queue_info[modification_index][:4] # the base info of the modification position
+                                for info in queue_info:
+                                    out_line.extend(info[4:])
+                                writer.writerow(out_line)
+                                have_write_something=True
+                                
+                        queue_base.pop(0)  # pop out the first site
+                        queue_position.pop(0)
+                        queue_info.pop(0)
 
-        feature_order=["gene_ID","position","base_type","coverage",'base_qual_mean_3', 'base_qual_mean_2', 'base_qual_mean_4', 'del_site_3', 'del_2', 'base_qual_mean_1', 'del_len_2', 'mis_3', 'mis_4', 'base_qual_mean_5', 'base_qual_STD_4', 'mis_T_4', 'mis_2', 'mis_5', 'mis_G_3', 'base_qual_STD_3', 'base_qual_STD_5', 'mis_A_2', 'base_qual_count_0_3', 'ins_2', 'mis_1', 'base_qual_count_0_2', 'base_qual_count_0_4', 'mis_T_3', 'del_site_2', 'mis_A_5', 'base_qual_STD_2', 'mis_C_2', 'mis_C_3', 'ins_len_4', 'base_qual_count_0_5', 'ins_4', 'mis_A_1', 'ins_len_2', 'del_1', 'base_qual_count_0_1', 'mis_A_4', 'del_site_4', 'del_len_1', 'base_qual_STD_1', 'ins_3', 'del_site_5', 'mis_C_5', 'mis_C_1', 'del_len_4', 'del_4', 'del_site_1', 'ins_len_3', 'ins_1', 'del_len_3', 'del_3', 'ins_len_1', 'mis_T_2', 'mis_G_4', 'mis_T_1', 'del_5', 'del_len_5', 'ins_5', 'ins_len_5', 'mis_G_5']
-        df=df.reindex(columns=feature_order)
-        df.to_csv(out_file,header=True,index=False)
-    else:
-        print("No A site available.")
+        # there are too many columns, let's generate the name of the columns
+        cols=["gene_ID","position","base_type","coverage"]
+        base_col=["ins","ins_len","del","del_len","del_site","mis","mis_A","mis_C","mis_G","mis_T","base_qual_mean","base_qual_STD","base_qual_count_0"]
+        for i in range(1,6):
+            for item in base_col:
+                cols.append(f"{item}_{i}")
+        # print(cols)
+        if have_write_something:
+            df=pd.read_csv(temp_file,header=None)
+            df.columns=cols
 
-    os.system("rm "+temp_file)
+            df.to_csv(out_file,header=True,index=False)
+        else:
+            print(f"No A site available in {motif}.")
+
+    os.system("rm -r "+temp_folder)
